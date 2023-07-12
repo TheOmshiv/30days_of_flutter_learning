966c7ad7519eae0e0bc1bc1023194d5fdbe2961e
TheOmshiv <omshiv_12017072@nitkkr.ac.in>
2023-07-12 16:31:32 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,26 @@
-// import 'package:flutter/material.dart';
-
-// void main(){
-//   runApp(MyApp());
-// }
-
-
-//  class MyApp extends StatelessWidget {
-//   const MyApp({ Key key}) : super(key key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Material(
-//         child: Center(
-//           child: Container(
-//             child: Text("Welcome to 10 days of flutter"),
-//           ),
-//         ),
-//       )
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
-  runApp(MyApp(key: UniqueKey()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({required Key key}) : super(key: key);
+  const MyApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 10 days of flutter"),
-          ),
-        ),
-      ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      darkTheme: ThemeData(primarySwatch: Colors.green),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => new LoginPage(),
+        "/home": (context) => HomePage(),
+        "/Login": (context) => LoginPage()
+      },
     );
   }
 }
-
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  // const homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Omshiv";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My first app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter with $name"),
+        ),
+      ),
+      drawer: Drawer(),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    );
+  }
+}
